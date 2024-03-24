1604882ef745d1b3a4d27fcd07603f06afe9a8a3
Elkadhi Oussama <83676401+elkadhi-oussama@users.noreply.github.com>
2024-03-24 22:51:34 +0100

    transfo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./CSS/App.css";
 import CribleJF100 from "./Pages/CribleJF100";
 import ETATSGENERAL from "./Pages/ETATSGENERAL";
+import Transfor from "./Pages/Transfor";
 
 function App() {
   return (
-    <div className="AllMyApp">
-      <CribleJF100 title={"CRIBLE JF 100"} />
-      <ETATSGENERAL title={"ETATS GENERAL"} />
-      <CribleJF100 title={"BROYEUR WB 100"} />
-    </div>
+    <>
+      {/* <div className="AllMyApp">
+        <CribleJF100 title={"CRIBLE JF 100"} />
+        <ETATSGENERAL title={"ETATS GENERAL"} />
+        <CribleJF100 title={"BROYEUR WB 100"} />
+      </div> */}
+      <Transfor />
+    </>
   );
 }
 
---
+++ src/Pages/Transfor.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBarTransfor from "../components/NavBarTransfor";
+import NavbarUser from "../components/NavbarUser";
+
+const Transfor = () => {
+  return (
+    <div>
+      <NavBarTransfor />
+      <NavbarUser />
+    </div>
+  );
+};
+
+export default Transfor;
