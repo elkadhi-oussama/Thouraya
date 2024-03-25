ae2e9e9697cadccdfeab411bf60aea5f630f3c84
Elkadhi Oussama <83676401+elkadhi-oussama@users.noreply.github.com>
2024-03-25 22:37:41 +0100

    trans

--- src/Pages/Transfor.js
+++ src/Pages/Transfor.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBarTransfor from "../components/NavBarTransfor";
 import NavbarUser from "../components/NavbarUser";
+import MainCompTransfor from "../components/MainCompTransfor";
 
 const Transfor = () => {
   return (
     <div>
       <NavBarTransfor />
       <NavbarUser />
+      <MainCompTransfor />
     </div>
   );
 };
